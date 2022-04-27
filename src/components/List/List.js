f0608625fc8af1790565584cb8946e27d0085ca9
miriacode <miriamacunaenciso@gmail.com>
2022-04-27 17:32:04 -0500

    Completed exercise

---
+++ src/components/List/List.js
@@ -0,0 +1,16 @@
+const List = () =>{ 
+    return(
+        <>
+        <h3>Things I need to do:</h3>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>    
+        </ul>
+        </>        
+
+    )
+}
+
+export default List;
